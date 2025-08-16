5c430415c4505a320005a99d17e6fdfd98b92635
Gnana guru <sgnanaguru31@gmail.com>
2025-08-17 02:07:56 +0530

    Overlay with transparency

--- src/simple.cpp
+++ src/simple.cpp
@@ -1,9 +1,11 @@
 #include "gtkmm/application.h"
 #include "gtkmm/button.h"
+#include "gtkmm/enums.h"
 #include "gtkmm/grid.h"
 #include "gtkmm/image.h"
-#include "gtkmm/overlay.h"
+#include "gtkmm/object.h"
 #include "gtkmm/window.h"
+#include <gtk4-layer-shell/gtk4-layer-shell.h>
 #include <gtkmm.h>
 #include "iostream"
 
@@ -59,7 +61,26 @@ class MyButton : public Gtk::Button {
 
 class MyWindow : public Gtk::Window {
   private:
+    Glib::RefPtr<Gtk::GestureClick> click_controller;
+
+    void                            on_click(int n_press, double x, double y) {
+        double gx, gy;
+        if (!grid.translate_coordinates(*this, 0, 0, gx, gy)) {
+            std::cerr << "Failed to translate coordinates!" << std::endl;
+            return;
+        }
+
+        int gw = grid.get_allocation().get_width();
+        int gh = grid.get_allocation().get_height();
+
+        if (!(x >= gx && x <= gx + gw && y >= gy && y <= gy + gh)) {
+            close();
+        }
+        // inside grid => do nothing, let widgets handle events
+    }
+
   public:
+    Gtk::Grid grid;
     MyWindow();
 };
 
@@ -68,31 +89,54 @@ MyWindow::MyWindow() {
     set_opacity(1);
     set_default_size(500, 140);
 
-    Gtk::Grid grid = Gtk::Grid();
     grid.set_margin(10);
     grid.set_size_request(BTN_WIDTH * 4, BTN_HEIGHT);
+    grid.set_valign(Gtk::Align::CENTER);
+    grid.set_halign(Gtk::Align::CENTER);
 
-    MyButton logoutBtn("./contents/icons/logout.png");
-    MyButton lockBtn("./contents/icons/lock.png");
-    MyButton rebootBtn("./contents/icons/user.png");
-    MyButton powerBtn("./contents/icons/power.png");
+    gtk_layer_init_for_window(gobj());
+    gtk_layer_set_layer(gobj(), GTK_LAYER_SHELL_LAYER_OVERLAY);
+    gtk_layer_set_anchor(gobj(), GTK_LAYER_SHELL_EDGE_TOP, TRUE);
+    gtk_layer_set_anchor(gobj(), GTK_LAYER_SHELL_EDGE_BOTTOM, TRUE);
+    gtk_layer_set_anchor(gobj(), GTK_LAYER_SHELL_EDGE_LEFT, TRUE);
+    gtk_layer_set_anchor(gobj(), GTK_LAYER_SHELL_EDGE_RIGHT, TRUE);
 
-    logoutBtn.signal_clicked().connect([&]() { logoutBtn.on_button_clicked(LOGOUT); });
-    lockBtn.signal_clicked().connect([&]() { lockBtn.on_button_clicked(LOCK); });
-    rebootBtn.signal_clicked().connect([&]() { rebootBtn.on_button_clicked(REBOOT); });
-    powerBtn.signal_clicked().connect([&]() { powerBtn.on_button_clicked(POWER); });
+    auto logoutBtn = Gtk::make_managed<MyButton>("./contents/icons/logout.png");
+    auto lockBtn   = Gtk::make_managed<MyButton>("./contents/icons/lock.png");
+    auto rebootBtn = Gtk::make_managed<MyButton>("./contents/icons/user.png");
+    auto powerBtn  = Gtk::make_managed<MyButton>("./contents/icons/power.png");
 
-    grid.attach(powerBtn, 0, 0);
-    grid.attach(logoutBtn, 1, 0);
-    grid.attach(lockBtn, 2, 0);
-    grid.attach(rebootBtn, 3, 0);
+    logoutBtn->signal_clicked().connect([&]() { logoutBtn->on_button_clicked(LOGOUT); });
+    lockBtn->signal_clicked().connect([&]() { lockBtn->on_button_clicked(LOCK); });
+    rebootBtn->signal_clicked().connect([&]() { rebootBtn->on_button_clicked(REBOOT); });
+    powerBtn->signal_clicked().connect([&]() { powerBtn->on_button_clicked(POWER); });
 
-    Gtk::Overlay overlay;
-    overlay.add_overlay(grid);
-    set_child(overlay);
+    grid.attach(*powerBtn, 0, 0);
+    grid.attach(*logoutBtn, 1, 0);
+    grid.attach(*lockBtn, 2, 0);
+    grid.attach(*rebootBtn, 3, 0);
+
+    click_controller = Gtk::GestureClick::create();
+    click_controller->signal_pressed().connect(sigc::mem_fun(*this, &MyWindow::on_click));
+    add_controller(click_controller);
+    set_child(grid);
+
+    // Style: transparent window + semi-transparent overlay
+    auto provider = Gtk::CssProvider::create();
+    provider->load_from_data(R"(
+            window {
+                background-color: transparent;
+            }
+            .background {
+                background-color: rgba(0, 0, 0, 0.4);
+            }
+        )");
+
+    get_style_context()->add_provider_for_display(Gdk::Display::get_default(), provider, GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
 }
 
 int main(int argc, char* argv[]) {
     auto app = Gtk::Application::create("com.guru.simple");
+
     return app->make_window_and_run<MyWindow>(argc, argv);
 }
